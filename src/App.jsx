c4ec51e33d4b49b830b70b3c6c853762dc7ad46e
jefferson <jeffersonwcr@gmail.com>
2023-10-10 21:33:51 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,60 +4,60 @@ import Clime from './component/Clime'
 
 
 function App() {
-  const [search, setSearch]=useState ('')
-  const [value,setValues]= useState('')
-  const [icon,setIcon]= useState('')
-  const [today,setToday]= useState({})
+  const [search, setSearch] = useState('')
+  const [value, setValues] = useState('')
+  const [icon, setIcon] = useState('')
+  const [today, setToday] = useState({})
 
-  const [isDay, setIsDay]=useState(true)
+  const [isDay, setIsDay] = useState(true)
 
 
-  function day(){
+  function day() {
     setIsDay(!isDay)
   }
   console.log(isDay)
-  const background=isDay?"/dia.jpg":"/noche.jpg"
-  document.body.style=`background-image: url(${background});
+  const background = isDay ? "/dia.jpg" : "/noche.jpg"
+  document.body.style = `background-image: url(${background});
   background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
         background-attachment: fixed;
         transition: 500ms;`
-  
-useEffect(() =>{
 
-  navigator.geolocation.getCurrentPosition(function(position){
-    const latitude = position.coords.latitude
-    const longitude = position.coords.longitude
+  useEffect(() => {
 
-    const appKey= `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d5dc6140fd1f0d48cabfc23ecd826105`
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const latitude = position.coords.latitude
+      const longitude = position.coords.longitude
 
-    axios
-    .get(appKey)
-    .then((resp) =>{
-       setToday(resp.data)
-    })
-    .catch((error) =>{
-        console.log (error)
-    })
-      },function(error){console.log(error)},{ enableHighAccuracy:true})
+      const appKey = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d5dc6140fd1f0d48cabfc23ecd826105`
+
+      axios
+        .get(appKey)
+        .then((resp) => {
+          setToday(resp.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }, function (error) { console.log(error) }, { enableHighAccuracy: true })
 
 
-  },[])
+  }, [])
 
 
 
   return (
     <>
-    <div className='cont'>
+      <div className='cont'>
         <button className='cont__OnOff' onClick={day}>🌞🌑</button>
-    </div>
-    <h1 className='title'>Weather App</h1>
-    
-   
-     <Clime goodDay={today}/>
-    
-         </>
+      </div>
+      <h1 className='title'>Weather App</h1>
+
+
+      <Clime goodDay={today} />
+
+    </>
   )
 }
 
